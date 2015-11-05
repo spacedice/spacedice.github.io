033b240588a6fa5174b7d2cd69103208fc44e716
spacedice <drtrick@gmx.co.uk>
2015-11-05 20:10:21 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
 var tmpHE = 0.0001 + 0.0099*Math.random();
 var config = {
   // - Your app's id on moneypot.com
-  app_id: 85,                             // <----------------------------- EDIT ME!
+  app_id: 721,                             // <----------------------------- EDIT ME!
   // - Displayed in the navbar
-  app_name: 'Quantum Dice',
+  app_name: 'Space Dice',
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
   recaptcha_sitekey: '6LdNvggTAAAAANd3cn-AD54gQFOYFu4Si3FYSPq0',  // <----- EDIT ME!
-  redirect_uri: 'https://qdice.net/',
+  redirect_uri: 'https://spacedice.github.io',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
   chat_uri: '//socket.moneypot.com',
